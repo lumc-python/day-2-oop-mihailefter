73e187e6e9156dcfdeabc3cd6d445ea1ed07e2f1
Mihai <M.Lefter@lumc.nl>
2022-11-30 17:15:55 +0100

    Add solutions

--- fraction.py
+++ fraction.py
@@ -1,3 +1,12 @@
+def gcd(a, b):
+    """
+    Compute the greatest common divider of a and b in a recursive manner.
+    """
+    if b == 0:
+        return a
+    return gcd(b, a % b)
+
+
 class Fraction(object):
     """
     A simple fraction class.
@@ -7,5 +16,89 @@ class Fraction(object):
         """
         Creates a new fraction.
         """
-        self._numerator = numerator
-        self._denominator = denominator
+        if isinstance(numerator, int) and isinstance(denominator, int):
+            self._numerator = numerator
+            self._denominator = denominator
+        else:
+            raise ValueError("Expecting integers.")
+
+    # 1. Add a member function to give a print representation for the class,
+    #    e.g., `print(Fraction(1, 2))` should yield in something like: `1/2`.
+    def __str__(self):
+        return str(self._numerator) + "/" + str(self._denominator)
+
+    # 2. Add a member function to add to fractions,
+    #    e.g., `print(Fraction(1, 2)+ Fraction(2, 3))` should yield `7/6`.
+    #    Remember: the return type should be chosen correctly, e.g., the
+    #    addition of two fractions results in a new fraction, not a string
+    #    representation of the result!
+    def __add__(self, other):
+        return Fraction(
+            self._numerator * other._denominator + other._numerator * self._denominator,
+            self._denominator * other._denominator,
+        )
+
+    # 3. Add a member function to invert a fraction, e.g.,
+    #   `print(Fraction(1, 2).invert())` should yield `2/1`.
+    def invert(self):
+        return Fraction(self._denominator, self._numerator)
+
+    # 4. Add a member function to give a decimal representation of a fraction,
+    #    e.g., `print(Fraction(1, 2).to_float())` should yield `0.5`.
+    def to_float(self):
+        return self._numerator / self._denominator
+
+    # 5. Add a member function that returns the *integer part* of a fraction,
+    #    e.g., `Fraction(7, 6).integer()` should yield `1`.
+    def integer(self):
+        return self._numerator // self._denominator
+
+    # 6. Add member functions to subtract, multiply and divide fractions.
+    #    Hint: can we reuse any of the member functions?
+    def __sub__(self, other):
+        return Fraction(
+            self._numerator * other._denominator - other._numerator * self._denominator,
+            self._denominator * other._denominator,
+        )
+
+    def __mul__(self, other):
+        if isinstance(other, Fraction):
+            return Fraction(
+                self._numerator * other._numerator,
+                self._denominator * other._denominator,
+            )
+        # 11. Can you add integer multiplication functionality, e.g.,
+        #     print(Fraction(1, 2) * 2) should yield 2/2.
+        elif isinstance(other, int):
+            return Fraction(self._numerator * other, self._denominator)
+
+    def __truediv__(self, other):
+        return self * other.invert()
+
+    # 7. Add a member function to simplify a fraction,
+    #    e.g., `print(Fraction(2, 6).simplify())` should yield `1/3`.
+    #    Hint: use a function to calculate the greatest common divisor.
+    def simplify(self):
+        divisor = gcd(self._numerator, self._denominator)
+        self._numerator = self._numerator // divisor
+        self._denominator = self._denominator // divisor
+        return self
+
+    # 9. Add some comparison operators, e.g., == and <.
+    def __eq__(self, other):
+        s_s = Fraction(self._numerator, self._denominator).simplify()
+        s_o = Fraction(other._numerator, other._denominator).simplify()
+        return s_s._denominator == s_o._denominator and s_s._numerator == s_o._numerator
+
+    def __lt__(self, other):
+        # return self.to_float() >= other.to_float()
+        return self._numerator * other._denominator < other._numerator * self._denominator
+
+    def __ge__(self, other):
+        # return self.to_float() >= other.to_float()
+        return self._numerator * other._denominator >= other._numerator * self._denominator
+
+    # 11. What about commutativity?
+    def __rmul__(self, other):
+        if isinstance(other, int):
+            return Fraction(self._numerator * other, self._denominator)
--- test_fraction.py
+++ test_fraction.py
@@ -1,4 +1,47 @@
 from fraction import Fraction
 
 if __name__ == '__main__':
-    f1 = Fraction(1, 2)
+    # 1
+    print(Fraction(1, 2))
+
+    # 2
+    print(Fraction(1, 2) + Fraction(2, 3))
+
+    # 3
+    print(Fraction(1, 2).invert())
+
+    # 4
+    print(Fraction(1, 2).to_float())
+
+    # 5
+    print(Fraction(7, 6).integer())
+
+    # 6
+    print(Fraction(1, 2) - Fraction(2, 3))
+    print(Fraction(1, 2) * Fraction(2, 3))
+    print(Fraction(1, 2) / Fraction(2, 3))
+
+    # 7
+    print(Fraction(2, 6).simplify())
+
+    # 8
+    print(Fraction(1, 2) * 2)
+
+    # 9
+    print(Fraction(1, 2) == Fraction(1, 2))
+    print(Fraction(1, 2) == Fraction(2, 4))
+    print(Fraction(1, 2) == Fraction(1, 4))
+
+    print(f"{Fraction(1, 2)} < {Fraction(1, 4)}:", Fraction(1, 2) < Fraction(1, 4))
+    print(f"{Fraction(1, 2)} < {Fraction(2, 4)}:", Fraction(1, 2) < Fraction(2, 4))
+    print(f"{Fraction(1, 2)} < {Fraction(3, 4)}:", Fraction(1, 2) < Fraction(3, 4))
+
+    print(f"{Fraction(1, 2)} >= {Fraction(1, 4)}:", Fraction(1, 2) >= Fraction(1, 4))
+    print(f"{Fraction(1, 2)} >= {Fraction(2, 4)}:", Fraction(1, 2) >= Fraction(2, 4))
+    print(f"{Fraction(1, 2)} >= {Fraction(3, 4)}:", Fraction(1, 2) >= Fraction(3, 4))
+
+    # 11
+    print(2 * Fraction(1, 2))
+
+    # 11
+    print(Fraction(0.4, 0.2))
